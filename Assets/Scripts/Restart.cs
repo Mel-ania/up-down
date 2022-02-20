1ea55ac99e478898e7dead797ecf46b1886409b7
Melania Gottardo <64848167+Mel-ania@users.noreply.github.com>
2022-02-20 21:25:51 +0100

    Add files via upload

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("Quit the game!");
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+    public Text scoreDisplay;
+    public Player player;
+
+    private void Update()
+    {
+        scoreDisplay.text = "SCORE " + score;
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.CompareTag("Obstacle") && player.health > 0)
+        {
+            score++;
+            Debug.Log(score);
+        }
+    }
+    
+}
